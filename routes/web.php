f2f33de90c23e0685a743c81cad6ed946814ef4e
Powppo <mafrizal575@gmail.com>
2022-02-22 14:18:11 +0700

    Jobsheet II, Practicum 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//JOBSHEET 2
+//Practicum 1
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hi, Welcome to Laravel ";
 });
+
+Route::get('/about', function () {
+    echo "2041720247 <br> ";
+    echo "Muhammad Afrizal Hamimudin <br> ";
+    echo "TI-2I ";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "This is Articles Pages with ID: ".$id;
+});
+
